ceabee2439ab52bcbfddf16d2ac78b8733dd4c6e
Anastasiya Lugovskaya <Nastacya.l@yandex.ru>
2024-04-07 22:47:37 +0300

    add functions.py with auxiliary static methods

---
+++ selenium_rr/functions/functions.py
@@ -0,0 +1,15 @@
+def get_names_of_goods(goods_list):
+    """this method is used to get names of goods in a list"""
+    name_items = [i.text for i in goods_list]
+    return name_items
+
+
+def get_prices_of_goods(goods_list):
+    """this method is used to get prices of goods in a list"""
+    good_prices = [float(i.text[1:]) for i in goods_list]
+    return good_prices
+
+
+def sort_list(lst: list, reverse_option):
+    """this method is used to sort a list"""
+    return sorted(lst, reverse=reverse_option)
