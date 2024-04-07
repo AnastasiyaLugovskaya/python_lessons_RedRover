6c9b8a9d3e02eadd58aa7a4f1a46144dc38a5ccb
Anastasiya Lugovskaya <Nastacya.l@yandex.ru>
2024-04-07 16:06:05 +0300

    add backpack page url to urls.py

--- selenium_rr/data/urls.py
+++ selenium_rr/data/urls.py
@@ -1,4 +1,5 @@
 class Url:
+    BACKPACK_PAGE_URL = 'https://www.saucedemo.com/inventory-item.html?id=4'
     BASE_URL = 'https://www.saucedemo.com/'
     BASKET_URL = 'https://www.saucedemo.com/cart.html'
     CATALOGUE_URL = 'https://www.saucedemo.com/inventory.html'
