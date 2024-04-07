bf772b33c2bed83331951d15db15063c2267c86e
Anastasiya Lugovskaya <Nastacya.l@yandex.ru>
2024-04-07 13:24:54 +0300

    add incorrect credentials to class UserData

--- selenium/data/user_data.py
+++ selenium/data/user_data.py
@@ -2,3 +2,5 @@ class UserData:
     ACCEPTED_USERNAMES = ['standard_user', 'problem_user', 'performance_glitch_user',
                           'error_user', 'visual_user']
     PASSWORD = 'secret_sauce'
+    INCORRECT_USERNAME = 'user'
+    INCORRECT_PASSWORD = 'password'
