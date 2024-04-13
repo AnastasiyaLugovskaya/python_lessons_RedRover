2919a51a107076919b5c3a2e469d4168f3c42628
Anastasiya Lugovskaya <Nastacya.l@yandex.ru>
2024-04-13 15:17:07 +0300

    refactor check_basket_padge_presense()method, add methods to interact with burger menu

--- selenium_rr/pages/header_menu.py
+++ selenium_rr/pages/header_menu.py
@@ -1,5 +1,6 @@
 from selenium_rr.locators.header_menu_locators import HeaderMenuLocators
 from selenium_rr.pages.base_page import BasePage
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 
 class HeaderMenu(BasePage):
@@ -10,11 +11,22 @@ class HeaderMenu(BasePage):
 
     def check_basket_badge_presence(self):
         """this method is used to check if there is a badge with numbers of added goods on the basket"""
-        return len(self.all_elements_are_visible(self.header_locators.BASKET_BADGES))
+        try:
+            return len(self.all_elements_are_visible(self.header_locators.BASKET_BADGES))
+        except (TimeoutException, NoSuchElementException):
+            return 0
+
+    def click_on_about_link(self):
+        """this method is used to move to about page"""
+        self.click_on_element(self.header_locators.ABOUT_LINK)
 
     def click_on_burger_menu(self):
         """this method is used to open burger menu"""
         self.click_on_element(self.header_locators.BURGER_MENU)
+
+    def click_on_reset_link(self):
+        """this method is used to click on a reset link in order to clear a basket"""
+        self.click_on_element(self.header_locators.RESET_LINK)
 
     def get_badge_value(self):
         """this method is used to get a number of added goods from a basket badge"""
