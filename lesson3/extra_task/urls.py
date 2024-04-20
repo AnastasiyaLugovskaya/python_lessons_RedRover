d9c2a0c3312b7a923b744651f4ebcea3fd337586
Anastasiya Lugovskaya <Nastacya.l@yandex.ru>
2024-04-20 16:12:27 +0300

    add broken page url to urls

--- lesson3/extra_task/urls.py
+++ lesson3/extra_task/urls.py
@@ -3,4 +3,4 @@ class Url:
     AUTH_PAGE_URL = 'https://the-internet.herokuapp.com/basic_auth'
     BASE_PAGE_URL = 'https://the-internet.herokuapp.com/'
     BASE_PAGE_URL_WITH_AUTH_DATA = 'http://admin:admin@the-internet.herokuapp.com/basic_auth'
-
+    BROKEN_IMAGE_PAGE_URL = 'https://the-internet.herokuapp.com/broken_images'
